4e3027c42f408254b064fa665c51dee53f28fdb8
CollinHandel <31261553+CollinHandel@users.noreply.github.com>
2020-02-10 10:12:09 -0500

    Finish MovieApp

---
+++ MovieApp/iOS/Custom/SQLite_iOS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using MovieApp.Custom;
+using SQLite.Net;
+using SQLite.Net.Platform.XamarinIOS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(MovieApp.iOS.Custom.SQLite_iOS))]
+namespace MovieApp.iOS.Custom
+{
+    public class SQLite_iOS:ISQLite
+    {
+        public SQLite_iOS()
+        {
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var libraryPath = Path.Combine(documentPath, "..", "Library");
+            var dbPath = Path.Combine(libraryPath, "myDatabase.db3");
+
+            return new SQLiteConnection(new SQLitePlatformIOS(), dbPath);
+        }
+    }
+}
