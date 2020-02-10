c1eeb7bd158647a75d65b3095ce29b94a053662f
CollinHandel <31261553+CollinHandel@users.noreply.github.com>
2020-02-10 10:08:39 -0500

    Finish Distance Converter

---
+++ DistanceConverter/DistanceConverter/DistanceConverterPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace DistanceConverter
+{
+    public partial class DistanceConverterPage : ContentPage
+    {
+        public DistanceConverterPage()
+        {
+            InitializeComponent();
+            Title = "Distance Converter";
+
+            ToolbarItem tbi = new ToolbarItem();
+            tbi.Text = "About";
+
+            tbi.Clicked += delegate {
+                Navigation.PushAsync(new Views.AboutPage());
+            };
+
+            this.ToolbarItems.Add(tbi);
+        }
+    }
+}
