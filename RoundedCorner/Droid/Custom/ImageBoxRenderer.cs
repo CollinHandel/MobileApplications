555f064fb3c9dfdb8ecd1495c8acd369ce6ae0b9
CollinHandel <31261553+CollinHandel@users.noreply.github.com>
2020-02-10 10:17:59 -0500

    Finish RoundedCorners

---
+++ RoundedCorner/Droid/Custom/ImageBoxRenderer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using RoundedCorners;
+using RoundedCorners.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ImageBox), typeof(ImageBoxRenderer))]
+namespace RoundedCorners.Droid
+{
+    public class ImageBoxRenderer: ImageRenderer
+    {
+        public ImageBoxRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override bool DrawChild(Android.Graphics.Canvas canvas, Android.Views.View child, long drawingTime)
+        {
+            try
+            {
+                // Clip path
+                var radius = 25.0f;
+                var path = new Path();
+                RectF rect = new RectF(0, 0, Width, Height);
+                path.AddRoundRect(rect, radius, radius, Path.Direction.Cw);
+                canvas.Save();
+                canvas.ClipPath(path);
+                var result = base.DrawChild(canvas, child, drawingTime);
+                canvas.Restore();
+
+                // Create Border
+                path = new Path();
+                path.AddRoundRect(rect, radius, radius, Path.Direction.Cw);
+                var paint = new Paint();
+                paint.AntiAlias = true;
+                paint.StrokeWidth = 1;
+                paint.SetStyle(Paint.Style.Stroke);
+                paint.Color = global::Android.Graphics.Color.Rgb(11, 11, 14);
+                canvas.DrawPath(path, paint);
+
+                // Clean Up
+                paint.Dispose();
+                path.Dispose();
+
+                return result;
+            } catch {}
+
+            return base.DrawChild(canvas, child, drawingTime);
+        }
+    }
+}
---
+++ RoundedCorner/iOS/Custom/ImageBoxRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RoundedCorners;
+using RoundedCorners.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ImageBox), typeof(ImageBoxRenderer))]
+namespace RoundedCorners.iOS
+{
+    public class ImageBoxRenderer : ImageRenderer
+    {
+        public ImageBoxRenderer()
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+                return;
+
+            CreateBox();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == VisualElement.HeightProperty.PropertyName || e.PropertyName == VisualElement.WidthProperty.PropertyName)
+                CreateBox();
+        }
+
+        private void CreateBox() {
+            try
+            {
+                Control.Layer.CornerRadius = 25;
+                Control.Layer.MasksToBounds = false;
+                Control.Layer.BorderColor = Xamarin.Forms.Color.FromHex("1B1D24").ToCGColor();
+                Control.Layer.BorderWidth = 1;
+                Control.ClipsToBounds = true;
+            } catch {}
+        }
+    }
+}
